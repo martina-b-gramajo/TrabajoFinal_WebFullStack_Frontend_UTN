dd802553f9dc689ae0c1178543e73810b10cbc10
martina_b_gramajo <martina.b.gramajo@gmail.com>
2025-02-11 20:06:38 -0300

    Frontend base

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ENVIROMENT from './utils/constants/enviroment'
+import { Route, Routes } from 'react-router-dom'
+import RegisterScreen from './screens/RegisterScreen'
+import LoginScreen from './screens/LoginScreen'
+import ErrorScreen from './screens/ErrorScreen'
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen'
+import ResetPasswordScreen from './screens/ResetPasswordScreen'
+import './app.css'
+import ProtectedRoute from './Components/ProtectedRoute'
+import HomeScreen from './screens/HomeScreen'
+import CreateWorkspaceScreen from './screens/CreateWorkspaceScreen'
+import WorkspaceScreen from './screens/WorkspaceScreen'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<LoginScreen />} />
+        <Route path='/register' element={<RegisterScreen />} />
+        <Route path='/login' element={<LoginScreen />} />
+        <Route path='/error' element={<ErrorScreen />} />
+        <Route path='/forgot-password' element={<ForgotPasswordScreen />} />
+        <Route path='/reset-password' element={<ResetPasswordScreen />} />
+        <Route element={<ProtectedRoute />} >
+          <Route path='/home' element={<HomeScreen />} />
+          <Route path='/workspace/new' element={<CreateWorkspaceScreen />} />
+          <Route path='/workspace/:workspace_id' element={<WorkspaceScreen />} />
+          <Route path='/workspace/:workspace_id/:channel_id' element={<WorkspaceScreen />} />
+        </Route>
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ENVIROMENT from '../utils/constants/enviroment'
+import { getAuthenticatedHeaders } from '../fetching/customHeaders'
+import { useFetch } from '../hooks/useFetch'
+import { Link } from 'react-router-dom'
+
+const HomeScreen = () => {
+    const {
+        data: workspace_response,
+        error: workspace_response_error,
+        loading: workspace_loading
+    } = useFetch(ENVIROMENT.API_URL + '/api/workspace', {
+        method: "GET",
+        headers: getAuthenticatedHeaders()
+    })
+    console.log(workspace_response)
+    return (
+        <div>
+            <h1>Bienvenido a la app {/* en un futuro podrian poner el nombre del usuario */}</h1>
+            <div>
+                <h2>Tus espacios de trabajo</h2>
+                <div>
+                    {
+                        workspace_loading
+                            ? <h2>Cargando</h2>
+                            : (
+                                workspace_response.data.workspaces.length ?
+                                    workspace_response.data.workspaces.map(workspace => {
+                                        return (
+                                            <div key={workspace._id}>
+                                                <h3>{workspace.name}</h3>
+                                                <Link to={`/workspace/${workspace._id}`}>Ir al workspace</Link>
+                                            </div>
+                                        )
+                                    })
+                                    : <h3>Aun no creaste ningun espacio de trabajo!</h3>
+                            )
+                    }
+                </div>
+            </div>
+            <div>
+                <span>Aun no tienes espacios de trabajo?</span>
+                <Link to='/workspace/new'>Crear un espacio de trabajo</Link>
+            </div>
+        </div>
+    )
+}
+
+export default HomeScreen
